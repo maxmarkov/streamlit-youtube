132454a54466a23720e22c142aac05abd1dc9e16
maksim.markov1 <maksim.markov@kapernikov.com>
2022-03-24 00:11:33 +0100

    access checker

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import requests
 from pytube import YouTube
 
 # https://www.youtube.com/watch?v=Ch5VhJzaoaI&list=LL&index=39&t=90s
@@ -10,7 +10,20 @@ def clear_text():
 def download(stream):
     stream.download(output_path='./')
 
-st.set_page_config(page_title="🚋 Youtube downloader", layout="wide")
+def can_access(url):
+    """ check whether you can access the video"""
+    access = False
+    if len(url) > 0 and requests.get(url).status_code==200:
+        try:
+            tube = YouTube(url)
+            #tubecheck_availability() -> must be None or empty
+            access=True
+        except:
+            pass
+    return access
+
+
+st.set_page_config(page_title=" Youtube downloader", layout="wide")
 
 # ====== SIDEBAR ======
 with st.sidebar:
@@ -18,12 +31,14 @@ with st.sidebar:
     st.title("Youtube download app")
 
     url = st.text_input("Insert your link here", key="url")
-    st.write(url)
 
-    if len(url) > 0:
+    if can_access(url):
+
         tube = YouTube(url)
+
         fmt = st.selectbox("Choose format:", ['audio', 'video'])
 
+        # === StreamQuery Object === #
         mime_types = set([t.mime_type for t in tube.streams if t.type==fmt])
         if len(mime_types)>0:
             mime_type = st.selectbox("Mime types:", mime_types)
@@ -49,20 +64,23 @@ with st.sidebar:
                         st.button("Download file", on_click=download(stream[0]))
 
 
-    st.button("Clear all address boxes", on_click=clear_text)
+        st.button("Clear all address boxes", on_click=clear_text)
 
-    st.info(
-        "This is an open source project and you are very welcome to contribute your "
-        "comments, questions, resources and apps as "
-        "[issues](https://github.com/maxmarkov/streamlit-youtube/issues) or "
-        "[pull requests](https://github.com/maxmarkov/streamlit-youtube/pulls) "
-        "to the [source code](https://github.com/maxmarkov/streamlit-youtube). "
-    )
+        st.info(
+            "This is an open source project and you are very welcome to contribute your "
+            "comments, questions, resources and apps as "
+            "[issues](https://github.com/maxmarkov/streamlit-youtube/issues) or "
+            "[pull requests](https://github.com/maxmarkov/streamlit-youtube/pulls) "
+            "to the [source code](https://github.com/maxmarkov/streamlit-youtube). "
+        )
 
 
 
 
 # ====== MAIN PAGE ======
 
-if len(url) > 0:
+if can_access(url):
     st.video(url)
+
+#if len(url) > 0 and requests.get(url).status_code!=200:
+#    st.write(requests.get(url))
