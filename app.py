a7c9557895dd8ad9aff958cff5b0ec2b3beae298
maksim.markov1 <maksim.markov@kapernikov.com>
2022-04-02 10:51:20 +0200

    check env variable

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def download_file(stream, fmt):
 
     stream.download(filename=title)
     
-    if ['DESKTOP_SESSION'] not in os.environ: #and os.environ('HOSTNAME')=='streamlit':
+    if 'DESKTOP_SESSION' not in os.environ: #and os.environ('HOSTNAME')=='streamlit':
     
         with open(title, 'rb') as f:
             bytes = f.read()
@@ -98,7 +98,7 @@ with st.sidebar:
         # === Download block === #
         if stream_quality is not None:
             stream_final = stream_quality[0]
-            if ['DESKTOP_SESSION'] in os.environ:
+            if 'DESKTOP_SESSION' in os.environ:
                 download = st.button("Download file", key='download')
             else:
                 download = st.button("Get download link", key='download')
